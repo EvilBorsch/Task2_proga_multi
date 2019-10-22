662752da2a3ea0611b32ecf297b57302e97f3ef3
EvilBorsch <44337169+EvilBorsch@users.noreply.github.com>
2019-10-22 23:47:05 +0300

    Update libdynamic.h

--- libdynamic.h
+++ libdynamic.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
+#include <stdint.h>
 
 void *summ_last_elements(void *stroki);
 
-long int find_sigma_diagonals(char **matrix1, size_t N);
+int16_t find_sigma_diagonals(char **matrix1, size_t N);
