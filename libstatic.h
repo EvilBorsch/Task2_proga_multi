e8d59037d7a097c980c1c50603b3570cf833b863
EvilBorsch <44337169+EvilBorsch@users.noreply.github.com>
2019-10-23 00:18:56 +0300

    Update libstatic.h

--- libstatic.h
+++ libstatic.h
@@ -4,11 +4,11 @@ char **init_matrix_from_keyboard(size_t n);
 
 char **init_with_num(size_t n, char num);
 
-void print_mat(char **mat, size_t n);
+void print_mat(const char **mat, size_t n);
 
 
 void del_mat(char **mat, size_t n);
 
-int get_sigma_diagonal(char **mat, size_t n);
+int get_sigma_diagonal(const char **mat, size_t n);
 
 
