5fe6e0fbde9cdae24191f7efb0d9aaf3b564535d
EvilBorsch <44337169+EvilBorsch@users.noreply.github.com>
2019-10-25 00:22:24 +0300

    Update libstatic.c

--- libstatic.c
+++ libstatic.c
@@ -1,77 +1,13 @@
 // Copyright 2019 EvilBorsch
 #include <stdio.h>
-#include <stdlib.h>
 #include "libstatic.h"
 
-char **init_matrix_from_keyboard(const size_t n) {
-  char **mat = (char **)malloc(n * sizeof(char *));
-  if (mat == NULL) {
-    perror("MEMORY ERROR");
-    return NULL;
-  }
-
-  size_t k = 0;
-
-  for (size_t i = 0; i < n; i++) {
-    mat[i] = (char *)malloc(sizeof(char) * (k + 1));
-    if (mat[i] == NULL) {
-      perror("MEMMORY ERROR");
-      free(mat);
-      return NULL;
-    }
-
-    for (size_t j = 0; j <= k; j++) {
-      char chis;
-      scanf("%c", &chis);
-      if (chis < 0 || chis > 3) {
-        free(mat[i]);
-        perror("BAD NUM");
-        return NULL;
-      }
-      mat[i][j] = chis;
-    }
-    k++;
-  }
-  return mat;
-}
-
-char **init_with_num(const size_t n, const char num) {
-  char **mat = (char **)malloc(n * sizeof(char *));
-  if (mat == NULL) {
-    perror("MEMMORY ERROR");
-    return NULL;
-  }
-  size_t k = 0;
-  for (size_t i = 0; i < n; i++) {
-    mat[i] = (char *)malloc(sizeof(char) * (k + 1));
-    if (mat[i] == NULL) {
-      perror("MEMMORY ERROR");
-      free(mat);
-      return NULL;
-    }
-    for (size_t j = 0; j <= k; j++) {
-      mat[i][j] = num;
-    }
-    k++;
-  }
-  return mat;
-}
-
-void del_mat(char **mat, const size_t n) {
-  if (mat == NULL) {
-    perror("mat is null");
-    return;
-  }
-  for (size_t i = 0; i < n; i++) {
-    free(mat[i]);
-  }
-  free(mat);
-}
+#define MEMMORY_ERROR 28
 
 int get_sigma_diagonal(char **mat, const size_t n) {
   if (mat == NULL) {
     perror("Mat is null");
-    return -1;
+    return MEMMORY_ERROR;
   }
   size_t sigma = 0;
   for (size_t i = 0; i < n; i++) {
