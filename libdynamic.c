e0dbbd0e8bb7ef327cd68b1ed34261c35314bb37
EvilBorsch <44337169+EvilBorsch@users.noreply.github.com>
2019-10-24 23:39:18 +0300

    Update libdynamic.c

--- libdynamic.c
+++ libdynamic.c
@@ -28,7 +28,7 @@ int summa_diagonali(int chisyad, char **matrix, int N) {
     previous_started_str = previous_started_str + N / chisyad;
 
     write(fd[1], buff,
-          sizeof(buff) + 1);  //передаем в процесс строку с которой начинаем
+          sizeof(buff) + 1);  // передаем в процесс строку с которой начинаем
     // подсчет элементов на диагонале для данного процесса
     int pid = fork();
     if (-1 == pid) {
@@ -37,20 +37,20 @@ int summa_diagonali(int chisyad, char **matrix, int N) {
     } else if (0 == pid) {
       int ans[1];
       int data[1];
-      read(fd[0], data, sizeof(data) + 1);  //Берем строку с которой считаем
+      read(fd[0], data, sizeof(data) + 1);  // Берем строку с которой считаем
       int process_started_str = data[0];
       ans[0] = process_summ_last_matrix_elements(process_started_str, matrix, N,
                                                  chisyad);
       close(fd[0]);
       write(fd[1], ans,
-            sizeof(ans) + 1);  //Кидаем в главный процесс посчитанную часть
+            sizeof(ans) + 1);  // Кидаем в главный процесс посчитанную часть
       exit(0);
     }
     waitpid(pid, &status, 0);
     int buf[1];
-    read(fd[0], buf, sizeof(buf));  //получаем из процесса посчитанную часть
+    read(fd[0], buf, sizeof(buf));  // получаем из процесса посчитанную часть
     // printf("Received %d\n", buf[0]);
-    okonch_result = okonch_result + buf[0];  //суммируем посчитанные части
+    okonch_result = okonch_result + buf[0];  // суммируем посчитанные части
     close(fd[1]);
   }
   return okonch_result;
