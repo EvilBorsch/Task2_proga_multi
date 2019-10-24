f505df00729014c5b03311b56f0e4e9d16ee1dbf
EvilBorsch <44337169+EvilBorsch@users.noreply.github.com>
2019-10-24 22:34:14 +0300

    test fix

--- libdynamic.c
+++ libdynamic.c
@@ -1,8 +1,9 @@
 // Copyright 2019 EvilBorsch
+#include <unistd.h>
 #include "libdynamic.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
 
 int process_summ_last_matrix_elements(int started_str, char **matrix, int N,
                                       int chisyad) {
@@ -29,7 +30,7 @@ int summa_diagonali(int chisyad, char **matrix, int N) {
     write(fd[1], buff,
           sizeof(buff) + 1);  //передаем в процесс строку с которой начинаем
     // подсчет элементов на диагонале для данного процесса
-    pid_t pid = fork();
+    int pid = fork();
     if (-1 == pid) {
       perror("Process Eroor");
       break;
