70211e895f59dabf07455ce299a183caf5b77004
EvilBorsch <44337169+EvilBorsch@users.noreply.github.com>
2019-10-26 00:07:04 +0300

    Update libdynamic.c

--- libdynamic.c
+++ libdynamic.c
@@ -1,15 +1,15 @@
 // Copyright 2019 EvilBorsch
-#include <unistd.h>
 #include "libdynamic.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 
 int process_summ_last_matrix_elements(int started_str, char **matrix, int N,
                                       int chisyad) {
   int result = 0;
   int kolvostrok = N / chisyad;
   if (started_str == (chisyad - 1) * kolvostrok) kolvostrok = N - started_str;
+
   for (int i = started_str; i < started_str + kolvostrok; i++) {
     result += matrix[i][i];
   }
@@ -19,39 +19,37 @@ int process_summ_last_matrix_elements(int started_str, char **matrix, int N,
 int summa_diagonali(int chisyad, char **matrix, int N) {
   int status;
   int okonch_result = 0;
-  int previous_started_str = 0;
-  for (int i = 0; i < chisyad; i++) {
-    int fd[2];
-    pipe(fd);
-    int buff[1];
-    buff[0] = previous_started_str;
-    previous_started_str = previous_started_str + N / chisyad;
 
-    write(fd[1], buff,
-          sizeof(buff) + 1);  // передаем в процесс строку с которой начинаем
-    // подсчет элементов на диагонале для данного процесса
-    int pid = fork();
+  int fd[2];
+  pipe(fd);
+  int pid = 0;
+  int started_str = 0;
+  for (int i = 0; i < N - chisyad; i += N / chisyad) {
+    pid = fork();
     if (-1 == pid) {
       perror("Process Eroor");
       break;
     } else if (0 == pid) {
-      int ans[1];
-      int data[1];
-      read(fd[0], data, sizeof(data) + 1);  // Берем строку с которой считаем
-      int process_started_str = data[0];
-      ans[0] = process_summ_last_matrix_elements(process_started_str, matrix, N,
-                                                 chisyad);
-      close(fd[0]);
-      write(fd[1], ans,
-            sizeof(ans) + 1);  // Кидаем в главный процесс посчитанную часть
-      exit(0);
+      started_str = i;
+      break;
     }
-    waitpid(pid, &status, 0);
-    int buf[1];
-    read(fd[0], buf, sizeof(buf));  // получаем из процесса посчитанную часть
-    // printf("Received %d\n", buf[0]);
-    okonch_result = okonch_result + buf[0];  // суммируем посчитанные части
-    close(fd[1]);
   }
+
+  if (pid == 0) {
+    // sleep(4);
+    int ans[1];
+    ans[0] = process_summ_last_matrix_elements(started_str, matrix, N, chisyad);
+    close(fd[0]);
+    write(fd[1], ans, sizeof(ans));
+    exit(0);
+  }
+
+  waitpid(pid, &status, 0);
+  int buf[chisyad];
+  read(fd[0], buf, sizeof(buf) * chisyad);
+  for (int i = 0; i < chisyad; i++) {
+    okonch_result = okonch_result + buf[i];
+  }
+  close(fd[1]);
   return okonch_result;
 }
