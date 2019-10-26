d77d452ea685cff70bc62af877c7f26b812f4f73
EvilBorsch <44337169+EvilBorsch@users.noreply.github.com>
2019-10-26 08:23:35 +0300

    major fixes

--- libdynamic.c
+++ libdynamic.c
@@ -1,62 +1,67 @@
 // Copyright 2019 EvilBorsch
+#include <errno.h>
 #include "libdynamic.h"
 #include <stdlib.h>
 #include <unistd.h>
 
-#define PROCESS_ERROR 11
-#define EMPTYMATRIX 15
-
-int process_summ_last_matrix_elements(int started_str, char **matrix, int N,
+int process_summ_last_matrix_elements(int started_str, char** matrix, int N,
                                       int chisyad) {
   if (matrix == NULL) {
-    exit(EMPTYMATRIX);
+    exit(EMSGSIZE);
   }
   int result = 0;
   int kolvostrok = N / chisyad;
-  if (started_str == (chisyad - 1) * kolvostrok) kolvostrok = N - started_str;
-
+  if (started_str == (chisyad - 1) * kolvostrok) {
+    kolvostrok = N - started_str;
+  }
+  printf("start: %d", started_str);
+  printf("kolvo: %d\n", kolvostrok);
   for (int i = started_str; i < started_str + kolvostrok; i++) {
     result += matrix[i][i];
   }
   return result;
 }
 
-int summa_diagonali(int chisyad, char **matrix, int N) {
-  if (matrix == NULL) {
-    exit(EMPTYMATRIX);
-  }
-  int status;
-  int okonch_result = 0;
-
+int summa_diagonali(int core_count, char** matrix, const int N) {
+  int pid[core_count];
+  int status = 0;
   int fd[2];
   pipe(fd);
-  int pid = 0;
-  int started_str = 0;
-  for (int i = 0; i <= (chisyad - 1) * N / chisyad; i += N / chisyad) {
-    pid = fork();
-    if (-1 == pid) {
-      exit(PROCESS_ERROR);
-    } else if (0 == pid) {
-      started_str = i;
-      break;
+  int d = 0;
+  for (int i = 0; i < N; i += N / core_count) {
+    pid[d] = fork();
+
+    if (pid[d] == -1) {
+      exit(EAGAIN);
     }
+
+    if (pid[d] == 0) {
+      close(fd[0]);
+      int* ans = malloc(sizeof(int));
+      ans[0] = process_summ_last_matrix_elements(i, matrix, N, core_count);
+
+      write(fd[1], ans, sizeof(int));
+      free(ans);
+      exit(EXIT_SUCCESS);
+    }
+    d++;
   }
 
-  if (pid == 0) {
-    int ans[1];
-    ans[0] = process_summ_last_matrix_elements(started_str, matrix, N, chisyad);
-    close(fd[0]);
-    write(fd[1], ans, sizeof(ans));
-    exit(0);
-  }
-
-  waitpid(pid, &status, 0);
-  int *buf = malloc(sizeof(int) * chisyad);
-  read(fd[0], buf, sizeof(buf) * chisyad);
-  for (int i = 0; i < chisyad; i++) {
-    okonch_result = okonch_result + buf[i];
+  for (size_t c = 0; c < core_count; ++c) {
+    waitpid(pid[c], &status, 0);
   }
   close(fd[1]);
+
+  int* buf = malloc(sizeof(int) * core_count);
+
+  int answer = 0;
+
+  read(fd[0], buf, core_count * sizeof(int));
+
+  for (size_t c = 0; c < core_count; ++c) {
+    answer += buf[c];
+  }
+
   free(buf);
-  return okonch_result;
+  return answer;
 }
